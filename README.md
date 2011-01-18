863843b1bcae6a11e9e7743e16851298125ff13a
JakeWharton <jakewharton@gmail.com>
2011-01-18 09:36:44 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,60 @@
+backupdeleter.py
+================
+`backupdeleter.py` is a python script which can be used for automatically
+deleting old backups files. The script reads a configuration file which defines
+the paths, patterns, and age of the files which should be deleted.
+
+By triggering this to run as a post-backup script or as a cron job you can ensure
+that you only keep the desired amount of backups necessary.
+
+
+Prerequisites
+-------------
+
+1.  Python 2.7.1+
+    http://python.org/
+
+    The python runtime is required for executing this script. A minimum of
+    version 2.7.1 is needed for the pattern matching libraries.
+
+
+Usage
+-----
+Executing this script requires passing in the configuration file as the only
+parameter. It will then parse the configuration and act accordingly on the
+directories contained therein.
+
+By default the script will output a log of any warnings or errors and any files
+that are deleted to `backupdeleter.log`. If you wish to see more information
+you can edit the script and change the logging level to `DEBUG`. To see only
+issues with the configuration change the logging level to `WARN`.
+
+You can see an example configuration file and the syntax that it uses in
+`config.txt.sample`.
+
+
+Developed By
+------------
+Champion International Moving, Ltd.
+
+* Jake Wharton - <jake@champmove.com>
+
+Git repository located at
+[http://github.com/ChampionInternationalMoving/backupdeleter](http://github.com/ChampionInternationalMoving/backupdeleter)
+
+
+License
+-------
+    Copyright 2011 Champion International Moving, Ltd.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ backupdeleter.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+'''
+Simple script to automatically delete old backups.
+'''
+
+__license__ = '''
+Copyright 2011 Champion International Moving, Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+from datetime import datetime
+import fnmatch
+import logging
+import os
+import sys
+
+
+#Configure logging
+#change level to DEBUG for more info, WARN for less
+logging.basicConfig(filename='backupdeleter.log', format='%(asctime)s %(name)s %(levelname)s %(message)s', level=logging.INFO)
+log = logging.getLogger('backupdeleter.py')
+
+
+#Parse configuration file
+log.debug('sys.argv == %s', repr(sys.argv))
+if len(sys.argv) != 2:
+    log.error('You must specify a configuration file path as the only argument.')
+    sys.exit(1)
+
+configs = []
+try:
+    with open(sys.argv[1], 'r') as cfgfile:
+        for cfgline in cfgfile:
+            #Skip blank lines and comments
+            if cfgline and (cfgline.startswith('#') or cfgline.strip() == ''):
+                log.debug('Found comment or blank line.')
+                log.debug('> %s', cfgline.rstrip('\n'))
+                continue
+
+            #Split into parts
+            cfgargs = map(str.strip, cfgline.split(','))
+
+            #Error-check parts
+            if len(cfgargs) != 3:
+                log.warn('Line does not contain three values. Should be "Path, Pattern, Length (in days)".')
+                log.warn('> %s', cfgline)
+                continue
+            elif not os.path.exists(cfgargs[0]):
+                log.warn('Path is invalid or does not exist.')
+                log.warn('> %s', cfgargs[0])
+                continue
+            elif '*' not in cfgargs[1] and '?' not in cfgargs[1]:
+                log.warn('No wildcard character in pattern will result in one or zero matches.')
+                log.warn('> %s', cfgargs[1])
+                continue
+
+            try:
+                a = int(cfgargs[2])
+                if a <= 1:
+                    raise ValueError
+            except ValueError:
+                log.warn('Specified length is not a valid integer.')
+                log.warn('> %s', cfgargs[2])
+                continue
+
+            #Add to valid configs
+            configs.append({
+                'path': cfgargs[0],
+                'pattern': cfgargs[1],
+                'length': int(cfgargs[2])
+            })
+            log.debug('Added new configuration.')
+            log.debug('> path = %s', cfgargs[0])
+            log.debug('> pattern = %s', cfgargs[1])
+            log.debug('> length = %s', cfgargs[2])
+except IOError:
+    log.error('Could not open configuration file.')
+    log.error('> %s', sys.argv[1])
+    sys.exit(1)
+
+
+#Run jobs
+for config in configs:
+    for dirfile in os.listdir(config['path']):
+        if fnmatch.fnmatch(dirfile, config['pattern']):
+            match = os.path.join(config['path'], dirfile)
+            if (datetime.now() - datetime.fromtimestamp(os.stat(match).st_mtime)).days >= config['length']:
+                log.info('Removing "%s".', dirfile)
+                try:
+                    pass
+                    #os.remove(match)
+                except IOError:
+                    log.error('Could not remove file "%s".', dirfile)
